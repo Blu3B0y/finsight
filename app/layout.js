707fc76f95076c28b8b66cb401ab3df139e9ed3c
praxy481 <praxy481@gmail.com>
2025-11-03 23:20:33 +0530

    user authentication mostly done

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,32 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from "next/font/google"; // Correct: Destructuring the Inter font function
 import "./globals.css";
-
+import Header from "../components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+ 
 const inter = Inter({subsets:["latin"]});
-
+ 
 export const metadata = {
   title: "Finsight",
   description: "Your Financial Buddy",
 };
-
+ 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={`${inter.className}`}>
-        {children}
+        {/* header */}
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        {/* footer */}
+        <footer className="bg-blue-50 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            <p>Made by Team FinSight</p>
+          </div>
+        </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
